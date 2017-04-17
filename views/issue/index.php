445304344c85e7d61dcb3a1d8f22d92d3c8c2a61
nico <nicolas.p.suarez@gmail.com>
2017-04-17 15:30:14 -0300

    added search filters on issue index

--- views/issue/index.php
+++ views/issue/index.php
@@ -1,6 +1,9 @@
 <?php
 
+use app\models\IssueType;
+use kartik\select2\Select2;
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
@@ -28,14 +31,66 @@ $this->params['breadcrumbs'][] = $this->title;
 				'attribute' => 'id',
 				'contentOptions' => ['style' => 'width: 100px;'],
 			],
-            'user_id',
-            'customer_id',
+            [
+				'label' => 'Usuario',
+				'value' => 'user.username',
+				'filter' => Select2::widget([
+					'initValueText' => $searchModel->user_id ? $searchModel->user->username : null,
+					'model' => $searchModel,
+					'attribute' => 'user_id',
+					'options' => ['placeholder' => 'Elegir usuario'],
+					'pluginOptions' => [
+						'allowClear' => true,
+						'minimumInputLength' => 1,
+						'ajax' => [
+							'url' => Url::to('/site/user-list'),
+						],
+					],
+				]),
+			],
+            [
+				'label' => 'Cliente',
+				'value' => 'customer.name',
+				'filter' => Select2::widget([
+					'initValueText' => $searchModel->customer_id ? $searchModel->customer->name : null,
+					'model' => $searchModel,
+					'attribute' => 'customer_id',
+					'options' => ['placeholder' => 'Elegir cliente'],
+					'pluginOptions' => [
+						'allowClear' => true,
+						'minimumInputLength' => 3,
+						'ajax' => [
+							'url' => Url::to('/customer/list'),
+						],
+					],
+				]),
+			],
             [
 				'attribute' => 'order_id',
 				'contentOptions' => ['style' => 'width: 100px;'],
 			],
-            'product_id',
-            'issue_type_id',
+            [
+				'label' => 'Producto',
+				'value' => 'product.gecom_desc',
+				'filter' => Select2::widget([
+					'initValueText' => $searchModel->product_id ? $searchModel->product->gecom_desc . ' (' . $searchModel->product->stock . ')' : null,
+					'model' => $searchModel,
+					'attribute' => 'product_id',
+					'options' => ['placeholder' => 'Elegir producto'],
+					'pluginOptions' => [
+						'allowClear' => true,
+						'minimumInputLength' => 3,
+						'ajax' => [
+							'url' => Url::to('/product/list'),
+						],
+					],
+				]),
+			],
+            [
+				'label' => 'Tipo',
+				'value' => 'issueType.name',
+				'filter' => Html::activeDropDownList($searchModel, 'issue_type_id', IssueType::getIdNameArray(), ['class' => 'form-control', 'prompt' => 'Elegir tipo']),
+			],
             'comment:ntext',
 
             ['class' => 'yii\grid\ActionColumn'],
