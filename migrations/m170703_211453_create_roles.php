9121518263af3b47c3ca0833909f9e013dfb0406
nico <nicolas.p.suarez@gmail.com>
2017-07-03 19:03:37 -0300

    added roles migration

---
+++ migrations/m170703_211453_create_roles.php
@@ -0,0 +1,30 @@
+<?php
+
+use dektrium\rbac\migrations\Migration;
+
+class m170703_211453_create_roles extends Migration
+{
+
+	# Added just for compat with standard migrations
+	public $db;
+
+	public function safeUp()
+	{
+		$this->createRole('admin', 'Administrador');
+		$this->createRole('management', 'Administración');
+		$this->createRole('customerSupport', 'Atención');
+		$this->createRole('depot', 'Depósito');
+		$this->createRole('salesman', 'Vendedor');
+		$this->createRole('credit', 'Crédito');
+	}
+
+	public function safeDown()
+	{
+		$this->removeItem('admin');
+		$this->removeItem('management');
+		$this->removeItem('customerSupport');
+		$this->removeItem('depot');
+		$this->removeItem('salesman');
+		$this->removeItem('credit');
+	}
+}
