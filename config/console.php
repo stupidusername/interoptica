93ba7638a5c162772ce1f004acf35493318c7b06
nico <nicolas.p.suarez@gmail.com>
2017-07-24 14:07:49 -0300

    added customizable YII_ENV and YII_DEBUG for console app

--- config/console.php
+++ config/console.php
@@ -4,43 +4,43 @@ $params = require(__DIR__ . '/params.php');
 $db = require(__DIR__ . '/db.php');
 
 $config = [
-    'id' => 'basic-console',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'controllerNamespace' => 'app\commands',
-    'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'log' => [
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'db' => $db,
-    ],
-    'params' => $params,
-    'modules' => [
+	'id' => 'basic-console',
+	'basePath' => dirname(__DIR__),
+	'bootstrap' => ['log'],
+	'controllerNamespace' => 'app\commands',
+	'components' => [
+		'cache' => [
+			'class' => YII_ENV == 'dev' ? 'yii\caching\DummyCache' : 'yii\caching\MemCache',
+		],
+		'log' => [
+			'targets' => [
+				[
+					'class' => 'yii\log\FileTarget',
+					'levels' => ['error', 'warning'],
+				],
+			],
+		],
+		'db' => $db,
+	],
+	'params' => $params,
+	'modules' => [
 		'rbac' => 'dektrium\rbac\RbacConsoleModule',
-    ],
+	],
     /*
     'controllerMap' => [
-        'fixture' => [ // Fixture generation command line.
-            'class' => 'yii\faker\FixtureController',
-        ],
+	'fixture' => [ // Fixture generation command line.
+	    'class' => 'yii\faker\FixtureController',
+	],
     ],
-    */
+     */
 ];
 
 if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
-    ];
+	// configuration adjustments for 'dev' environment
+	$config['bootstrap'][] = 'gii';
+	$config['modules']['gii'] = [
+		'class' => 'yii\gii\Module',
+	];
 }
 
 return $config;
