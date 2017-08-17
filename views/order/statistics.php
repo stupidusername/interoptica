878e22701ee1fb63857135f7db936bdb92162a25
nico <nicolas.p.suarez@gmail.com>
2017-08-17 17:59:55 -0300

    added objective to order statistics

--- views/order/statistics.php
+++ views/order/statistics.php
@@ -1,5 +1,6 @@
 <?php
 
+use app\models\MonthlySummary;
 use app\models\OrderSummary;
 use app\models\User;
 use miloschuman\highcharts\Highcharts;
@@ -40,17 +41,34 @@ while ($period < $orderModel->queryToDate) {
 }
 $userIds = Yii::$app->authManager->getUserIdsByRole('salesman');
 $users = User::find()->andWhere(['id' => $userIds])->indexBy('id')->all();
+if ($orderModel->period == OrderSummary::PERIOD_MONTH) {
+	$objectives = MonthlySummary::find()->select(['user_id', 'period' => 'SUBDATE(begin_date, DAYOFMONTH(begin_date) - 1)', 'objective'])
+		->andWhere(['>=', 'begin_date', $orderModel->queryFromDate])->andWhere(['<', 'begin_date', $orderModel->queryToDate])
+		->andWhere(['user_id' => $userIds])->asArray()->indexBy(function ($row) { return $row['user_id'] . '-' . $row['period']; })->all();
+}
 $series = [];
 foreach ($userIds as $k => $id) {
-	$series[$k] = ['name' => $users[$id]->username, 'data' => [], 'key' => $id];
+	$series[$k * 2] = ['type' => 'column', 'name' => $users[$id]->username, 'data' => [], 'key' => $id];
+	if ($orderModel->period == OrderSummary::PERIOD_MONTH) {
+		$series[$k * 2 + 1] = ['type' => 'errorbar', 'name' => 'Objetivo', 'data' => []];
+	}
 	foreach ($periods as $period) {
-		$series[$k]['data'][] = isset($ordersBySalesman[$id . '-' . $period]) ? (int) $ordersBySalesman[$id . '-' . $period]->totalQuantity : 0;
+		$idx = $id . '-' . $period;
+		$quantity = isset($ordersBySalesman[$idx]) ? (int) $ordersBySalesman[$idx]->totalQuantity : 0;
+		$series[$k * 2]['data'][] = $quantity;
+		if ($orderModel->period == OrderSummary::PERIOD_MONTH) {
+			$objective = isset($objectives[$idx]) ? (int) $objectives[$idx]['objective'] : 0;
+			$series[$k * 2 + 1]['data'][] = [$quantity, $objective];
+		}
 	}
 }
 ?>
 
 <?=
 Highcharts::widget([
+	'scripts' => [
+		'highcharts-more',
+	],
 	'setupOptions' => [
 		'lang' => [
 			'decimalPoint' => ',',
@@ -61,7 +79,7 @@ Highcharts::widget([
 		'chart' => ['type' => 'column'],
 		'title' => ['text' => 'Ventas'],
 		'xAxis' => ['categories' => $periods],
-		'yAxis' => ['title' => ['text' => 'Piezas vendidas']],
+		'yAxis' => ['title' => ['text' => 'Piezas']],
 		'plotOptions' => [
 			'column' => [
 				'dataLabels' => ['enabled' => true],
@@ -75,7 +93,7 @@ Highcharts::widget([
 				],
 			],
 		],
-		'series' => $series,
+		'series' => array_values($series),
 	],
 ]);
 ?>
