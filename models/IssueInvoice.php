5859f9355a964f152c7ea5013a4235a26c1f8c4e
nico <nicolas.p.suarez@gmail.com>
2017-09-17 18:40:24 -0300

    added invoice models

---
+++ models/IssueInvoice.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+use app\validators\InvoiceValidator;
+use Yii;
+
+/**
+ * This is the model class for table "issue_invoice".
+ *
+ * @property integer $id
+ * @property integer $issue_id
+ * @property string $number
+ *
+ * @property Issue $issue
+ */
+class IssueInvoice extends \yii\db\ActiveRecord
+{
+	/**
+	 * @inheritdoc
+	 */
+	public static function tableName()
+	{
+		return 'issue_invoice';
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function rules()
+	{
+		return [
+			[['issue_id', 'number'], 'required'],
+			[['issue_id'], 'integer'],
+			[['number'], 'unique'],
+			[['number'], InvoiceValidator::className()],
+			[['issue_id'], 'exist', 'skipOnError' => true, 'targetClass' => Issue::className(), 'targetAttribute' => ['issue_id' => 'id']],
+		];
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'id' => 'ID',
+			'issue_id' => 'ID Reclamo',
+			'number' => 'Número de Factura',
+		];
+	}
+
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getIssue()
+	{
+		return $this->hasOne(Issue::className(), ['id' => 'issue_id']);
+	}
+}
---
+++ models/OrderInvoice.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+use app\validators\InvoiceValidator;
+use Yii;
+
+/**
+ * This is the model class for table "order_invoice".
+ *
+ * @property integer $id
+ * @property integer $order_id
+ * @property string $number
+ *
+ * @property Order $order
+ */
+class OrderInvoice extends \yii\db\ActiveRecord
+{
+	/**
+	 * @inheritdoc
+	 */
+	public static function tableName()
+	{
+		return 'order_invoice';
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function rules()
+	{
+		return [
+			[['order_id', 'number'], 'required'],
+			[['order_id'], 'integer'],
+			[['number'], 'unique'],
+			[['number'], InvoiceValidator::className()],
+			[['order_id'], 'exist', 'skipOnError' => true, 'targetClass' => Order::className(), 'targetAttribute' => ['order_id' => 'id']],
+		];
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function attributeLabels()
+	{
+		return [
+			'id' => 'ID',
+			'order_id' => 'ID Pedido',
+			'number' => 'Número de Factura',
+		];
+	}
+
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getOrder()
+	{
+		return $this->hasOne(Order::className(), ['id' => 'order_id']);
+	}
+}
