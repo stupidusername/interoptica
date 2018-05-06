7158661e3a104614bc7178c1ddeaff059e69366a
nico <nicolas.p.suarez@gmail.com>
2018-05-06 09:21:41 -0300

    fixed bower and npm errors

--- config/web.php
+++ config/web.php
@@ -97,6 +97,13 @@ $config = [
 		],
 	],
 	'params' => $params,
+	'aliases' => [
+		'@images' => '@app/web/images',
+		'@songs' => '@app/web/songs',
+		'@apks' => '@app/web/apks',
+		'@bower' => '@vendor/bower-asset',
+		'@npm'   => '@vendor/npm-asset',
+	],
 ];
 
 if (YII_ENV_DEV) {
