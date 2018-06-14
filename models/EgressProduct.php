015982238dd358b4b0680ae19629b230b23db995
nico <nicolas.p.suarez@gmail.com>
2018-06-14 06:51:02 -0300

    added EgressProduct model

--- models/EgressProduct.php
+++ models/EgressProduct.php
@@ -3,6 +3,8 @@
 namespace app\models;
 
 use Yii;
+use yii\helpers\Html;
+use yii\validators\UniqueValidator;
 
 /**
  * This is the model class for table "egress_product".
@@ -17,6 +19,8 @@ use Yii;
  */
 class EgressProduct extends \yii\db\ActiveRecord
 {
+    public $quantity;
+
     /**
      * @inheritdoc
      */
@@ -26,16 +30,51 @@ class EgressProduct extends \yii\db\ActiveRecord
     }
 
     /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [
-            [['egress_id', 'product_id'], 'integer'],
-            [['egress_id'], 'exist', 'skipOnError' => true, 'targetClass' => Egress::className(), 'targetAttribute' => ['egress_id' => 'id']],
-            [['product_id'], 'exist', 'skipOnError' => true, 'targetClass' => Product::className(), 'targetAttribute' => ['product_id' => 'id']],
-        ];
-    }
+  	* @inheritdoc
+  	*/
+  	public static function find() {
+  		$subquery = EgressProductBatch::find()->select('SUM(quantity)')->andWhere('eggress_product_id='. self::tableName() . '.id')->groupBy('egress_product_id');
+  		return parent::find()->addSelect([self::tableName() . '.*', 'quantity' => $subquery]);
+  	}
+
+    /**
+  	 * @inheritdoc
+  	 */
+  	public function afterSave($insert, $changedAttributes) {
+  		// Update product stock
+  		if (!$insert) {
+  			// Restore stock during update
+  			$this->restoreStock(true);
+  		}
+  		$this->updateStock();
+  		parent::afterSave($insert, $changedAttributes);
+  	}
+
+    /**
+  	 * @inheritdoc
+  	 */
+  	public function beforeDelete() {
+  		if (!parent::beforeDelete()) {
+  				return false;
+  		}
+  		$this->restoreStock(true);
+  		return true;
+  	}
+
+    /**
+  	 * @inheritdoc
+  	 */
+  	public function rules()
+  	{
+  		return [
+  			[['product_id', 'quantity'], 'required'],
+  			[['product_id'], 'integer'],
+  			[['quantity'], 'integer', 'min' => 1],
+  			[['product_id'], 'exist', 'skipOnError' => true, 'targetClass' => Product::className(), 'targetAttribute' => ['product_id' => 'id']],
+  			[['product_id'], 'uniqueEntry'],
+  			[['quantity'], 'inStock'],
+  		];
+  	}
 
     /**
      * @inheritdoc
@@ -44,8 +83,9 @@ class EgressProduct extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
-            'egress_id' => 'Egress ID',
-            'product_id' => 'Product ID',
+            'egress_id' => 'ID Egreso',
+            'product_id' => 'ID Producto',
+            'quantity' => 'cantidad',
         ];
     }
 
@@ -72,4 +112,99 @@ class EgressProduct extends \yii\db\ActiveRecord
     {
         return $this->hasMany(EgressProductBatch::className(), ['egress_product_id' => 'id']);
     }
+
+    /**
+  	 * Validates that entered quantity it's available
+  	 * @param string $attribute the attribute currently being validated
+  	 * @param mixed $params the value of the "params" given in the rule
+  	 * @param \yii\validators\InlineValidator related InlineValidator instance.
+  	 * This parameter is available since version 2.0.11.
+  	 */
+  	public function inStock($attribute, $param, $validator) {
+  		$stock = $this->product->stock !== null ? $this->product->stock : 0;
+  		$realStock = $stock;
+  		if (isset($this->oldAttributes['product_id']) && $this->product_id == $this->oldAttributes['product_id']) {
+  			$realStock += $this->getEgressProductBatches()->sum('quantity');
+  		}
+  		if ($this->$attribute > $realStock) {
+  			$this->addError($attribute, "El stock de este producto es de $realStock y tienen que quedar " . Product::STOCK_MIN. ".");
+  		}
+  	}
+
+    /**
+  	 * Validates that entered entry it's unique
+  	 * @param string $attribute the attribute currently being validated
+  	 * @param mixed $params the value of the "params" given in the rule
+  	 * @param \yii\validators\InlineValidator related InlineValidator instance.
+  	 * This parameter is available since version 2.0.11.
+  	 */
+  	public function uniqueEntry($attribute, $param, $validator) {
+  		$validator = new UniqueValidator([
+  			'targetAttribute' => ['egress_id', 'product_id'],
+  		]);
+  		$validator->validateAttribute($this, $attribute);
+  		if ($this->hasErrors($attribute)) {
+  			$this->clearErrors($attribute);
+  			$this->addError($attribute, 'El producto ya se encuentra en el egreso. ' .
+  				Html::a('Editar', ['/egress/update-entry', 'egressId' => $this->egress_id, 'productId' => $this->product_id], ['class' => 'productUpdate']) . '.');
+  		}
+    }
+
+    /**
+  	 * Restores stock in case of record deletion.
+  	 */
+  	public function restoreStock($delete = false) {
+  		$egressProductBatches = $this->getEgressProductBatches()->with(['batch'])->all();
+  		foreach ($egressProductBatches as $egressProductBatch) {
+  			$eggressProductBatch->batch->updatestock($egressProductBatch->quantity);
+  			if ($delete) {
+  				$egressProductBatch->delete();
+  			}
+  		}
+  	}
+
+  	/**
+  	* Update product stock
+  	* @param integer $productId
+  	* @param integer $quantity
+  	*/
+  	private function updateStock() {
+  		$quantity = $this->quantity;
+  		$batches = Batch::find()->andWhere(['product_id' => $this->product_id])->andWhere(['>', 'stock', 0])->orderBy(['id' => SORT_ASC])->all();
+  		foreach ($batches as $batch) {
+  			if ($quantity <= $batch->stock) {
+  				$removedQuantity = $quantity;
+  			} else {
+  				$removedQuantity = $batch->stock;
+  			}
+  			$batch->updateStock(-$removedQuantity);
+  			$egressProductBatch = new EgressProductBatch();
+  			$egressProductBatch->egress_product_id = $this->id;
+  			$egressProductBatch->batch_id = $batch->id;
+  			$egressProductBatch->quantity = $removedQuantity;
+  			$egressProductBatch->save(false);
+  			$quantity -= $removedQuantity;
+  			if ($quantity <= 0) {
+  				break;
+  			}
+  		}
+  		if ($quantity) {
+  			$batch = Batch::find()->andWhere(['product_id' => $this->product_id])->orderBy(['id' => SORT_DESC])->one();
+  			if ($batch) {
+  				$batch->updateStock(-$quantity);
+  			} else {
+  				$batch = new Batch();
+  				$batch->product_id = $this->product_id;
+  				$batch->entered_date = gmdate('Y-m-d');
+  				$batch->quantity = 0;
+  				$batch->stock = -$quantity;
+  				$batch->save(false);
+  			}
+  			$egressProductBatch = new EgressProductBatch();
+  			$egressProductBatch->egress_product_id = $this->id;
+  			$egressProductBatch->batch_id = $batch->id;
+  			$egressProductBatch->quantity = $quantity;
+  			$egressProductBatch->save(false);
+  		}
+  	}
 }
